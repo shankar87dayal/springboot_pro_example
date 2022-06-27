4d092819b93407727f2150f8ca7e6213b0b85090
shankar87dayal <shankar.87.dayal@gmail.com>
2022-06-27 23:31:23 +0530

    create spring boot project and add the spring starter web dependency and make one controller for testing this project..

---
+++ src/main/java/com/spring_boot/example/SpringbootProExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot/example/SpringbootProExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
