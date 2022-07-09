db8e9efbf8eca6595840f3391cdc5737c04015bf
shankar87dayal <shankar.87.dayal@gmail.com>
2022-07-09 23:22:12 +0530

    these class are not use so these class all line comments

--- src/main/java/com/spring_boot/example/serviceimpl/ProductServiceImpl.java
+++ src/main/java/com/spring_boot/example/serviceimpl/ProductServiceImpl.java
@@ -1,80 +1,80 @@
-package com.spring_boot.example.serviceimpl;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
-import com.spring_boot.example.entities.Product;
-import com.spring_boot.example.service.ProductService;
-
-@Service
-public class ProductServiceImpl implements ProductService{
-
-	List<Product> list = new ArrayList<>();
-	
-	@Override
-	public Product createProduct(Product product) {
-		// TODO Auto-generated method stub
-
-		System.out.println(product.getProductName());
-
-		list.add(product);
-		return product;
-
-	}
-
-	@Override
-	public List<Product> getAllProducts() {
-		// TODO Auto-generated method stub
-		return list;
-	}
-
-	@Override
-	public Product getProduct(int productId) {
-		// TODO Auto-generated method stub
-		
-
-		Product product = list.stream().filter(p->p.getProductId()==productId).findFirst().get();
-		System.out.println(product.getProductName());
-		return product;
-	}
-
-	@Override
-	public Product updateProduct(Product newProduct, int productId) {
-		// TODO Auto-generated method stub
-		List<Product> updatedList = list.stream().map(p->
-		{
-			if(p.getProductId()==productId)
-			{
-				p.setProductName(newProduct.getProductName());
-				p.setProductDesc(newProduct.getProductDesc());
-				p.setProductPrice(newProduct.getProductPrice());
-				p.setStock(newProduct.isStock());
-				
-				return p;
-			}else
-			{
-				return p;
-			}
-			
-			
-		}).collect(Collectors.toList());
-		
-		list =updatedList;
-		newProduct.setProductId(productId);
-		return newProduct;
-	}
-
-	@Override
-	public void deleteProduct(int productId) {
-		// TODO Auto-generated method stub
-		List<Product> newList = list.stream().filter(p->p.getProductId()!=productId).collect(Collectors.toList());
-		list=newList;
-		
-	}
-
-	
-	
-}
+//package com.spring_boot.example.serviceimpl;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//import org.springframework.stereotype.Service;
+//
+//import com.spring_boot.example.entities.Product;
+//import com.spring_boot.example.service.ProductService;
+//
+//@Service
+//public class ProductServiceImpl implements ProductService{
+//
+//	List<Product> list = new ArrayList<>();
+//	
+//	@Override
+//	public Product createProduct(Product product) {
+//		// TODO Auto-generated method stub
+//
+//		System.out.println(product.getProductName());
+//
+//		list.add(product);
+//		return product;
+//
+//	}
+//
+//	@Override
+//	public List<Product> getAllProducts() {
+//		// TODO Auto-generated method stub
+//		return list;
+//	}
+//
+//	@Override
+//	public Product getProduct(int productId) {
+//		// TODO Auto-generated method stub
+//		
+//
+//		Product product = list.stream().filter(p->p.getProductId()==productId).findFirst().get();
+//		System.out.println(product.getProductName());
+//		return product;
+//	}
+//
+//	@Override
+//	public Product updateProduct(Product newProduct, int productId) {
+//		// TODO Auto-generated method stub
+//		List<Product> updatedList = list.stream().map(p->
+//		{
+//			if(p.getProductId()==productId)
+//			{
+//				p.setProductName(newProduct.getProductName());
+//				p.setProductDesc(newProduct.getProductDesc());
+//				p.setProductPrice(newProduct.getProductPrice());
+//				p.setStock(newProduct.isStock());
+//				
+//				return p;
+//			}else
+//			{
+//				return p;
+//			}
+//			
+//			
+//		}).collect(Collectors.toList());
+//		
+//		list =updatedList;
+//		newProduct.setProductId(productId);
+//		return newProduct;
+//	}
+//
+//	@Override
+//	public void deleteProduct(int productId) {
+//		// TODO Auto-generated method stub
+//		List<Product> newList = list.stream().filter(p->p.getProductId()!=productId).collect(Collectors.toList());
+//		list=newList;
+//		
+//	}
+//
+//	
+//	
+//}
--- src/main/java/com/spring_boot/example/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/spring_boot/example/serviceimpl/UserServiceImpl.java
@@ -1,82 +1,82 @@
-package com.spring_boot.example.serviceimpl;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-
-import com.spring_boot.example.entities.User;
-import com.spring_boot.example.service.UserService;
-
-@Service
-public class UserServiceImpl implements UserService{
-
-	List<User> list = new ArrayList<>();
-	
-	
-	//create
-	
-	@Override
-	public User createUser(User user) {
-		// TODO Auto-generated method stub
-		System.out.println(user.getName());
-		list.add(user);
-		return user;
-	}
-
-	//get All
-	
-	@Override
-	public List<User> getAllUser() {
-		// TODO Auto-generated method stub
-		
-		return list;
-	}
-
-	//get single
-	
-	@Override
-	public User getUser(int userId) {
-		// TODO Auto-generated method stub
-		User user = list.stream().filter(u->u.getUserId() == userId).findFirst().get();
-		System.out.println(user.getName());
-		return user;
-	}
-
-	//update
-	
-	@Override
-	public User updateUser(int userId, User newUser) {
-		// TODO Auto-generated method stub
-		List<User> updatedUser = list.stream().map(u->
-		{
-			if(u.getUserId() == userId)
-			{
-				u.setName(newUser.getName());
-				u.setEmail(newUser.getEmail());
-				u.setPassword(newUser.getPassword());
-				u.setCity(newUser.getCity());
-				u.setAbout(newUser.getAbout());
-				
-				return u;
-			}else {
-				return u;
-			}
-		}).collect(Collectors.toList());
-		list=updatedUser;
-		newUser.setUserId(userId);
-		return newUser;
-	}
-
-	//delete
-	
-	@Override
-	public void deleteUser(int userId) {
-		// TODO Auto-generated method stub
-		
-		List<User> newlist = list.stream().filter(u->u.getUserId() != userId).collect(Collectors.toList());
-		list = newlist;
-	}
-
-}
+//package com.spring_boot.example.serviceimpl;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//import org.springframework.stereotype.Service;
+//
+//import com.spring_boot.example.entities.User;
+//import com.spring_boot.example.service.UserService;
+//
+//@Service
+//public class UserServiceImpl implements UserService{
+//
+//	List<User> list = new ArrayList<>();
+//	
+//	
+//	//create
+//	
+//	@Override
+//	public User createUser(User user) {
+//		// TODO Auto-generated method stub
+//		System.out.println(user.getName());
+//		list.add(user);
+//		return user;
+//	}
+//
+//	//get All
+//	
+//	@Override
+//	public List<User> getAllUser() {
+//		// TODO Auto-generated method stub
+//		
+//		return list;
+//	}
+//
+//	//get single
+//	
+//	@Override
+//	public User getUser(int userId) {
+//		// TODO Auto-generated method stub
+//		User user = list.stream().filter(u->u.getUserId() == userId).findFirst().get();
+//		System.out.println(user.getName());
+//		return user;
+//	}
+//
+//	//update
+//	
+//	@Override
+//	public User updateUser(int userId, User newUser) {
+//		// TODO Auto-generated method stub
+//		List<User> updatedUser = list.stream().map(u->
+//		{
+//			if(u.getUserId() == userId)
+//			{
+//				u.setName(newUser.getName());
+//				u.setEmail(newUser.getEmail());
+//				u.setPassword(newUser.getPassword());
+//				u.setCity(newUser.getCity());
+//				u.setAbout(newUser.getAbout());
+//				
+//				return u;
+//			}else {
+//				return u;
+//			}
+//		}).collect(Collectors.toList());
+//		list=updatedUser;
+//		newUser.setUserId(userId);
+//		return newUser;
+//	}
+//
+//	//delete
+//	
+//	@Override
+//	public void deleteUser(int userId) {
+//		// TODO Auto-generated method stub
+//		
+//		List<User> newlist = list.stream().filter(u->u.getUserId() != userId).collect(Collectors.toList());
+//		list = newlist;
+//	}
+//
+//}
