a0d767ddaafb0924d3cde1adfd188d9268bb66b3
shankar87dayal <shankar.87.dayal@gmail.com>
2022-07-09 10:22:22 +0530

    All classes are not use right now because these  class are use jdbc  for connecting the database.. and now i am using orm, jpa , hibernate

--- src/main/java/com/spring_boot/example/helper/CategoryRowMapper.java
+++ src/main/java/com/spring_boot/example/helper/CategoryRowMapper.java
@@ -1,24 +1,24 @@
-package com.spring_boot.example.helper;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.springframework.jdbc.core.RowMapper;
-
-import com.spring_boot.example.entities.Category;
-
-public class CategoryRowMapper implements RowMapper<Category>{
-
-	@Override
-	public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
-		// TODO Auto-generated method stub
-		
-		Category c =new Category();
-		c.setCategoryId(rs.getInt("categoryId"));
-		c.setTitle(rs.getString("title"));
-		c.setCategoryDesc(rs.getString("categoryDesc"));
-		return c;
-	}
-	
-
-}
+//package com.spring_boot.example.helper;
+//
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//import org.springframework.jdbc.core.RowMapper;
+//
+//import com.spring_boot.example.entities.Category;
+//
+//public class CategoryRowMapper implements RowMapper<Category>{
+//
+//	@Override
+//	public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+//		// TODO Auto-generated method stub
+//		
+//		Category c =new Category();
+//		c.setCategoryId(rs.getInt("categoryId"));
+//		c.setTitle(rs.getString("title"));
+//		c.setCategoryDesc(rs.getString("categoryDesc"));
+//		return c;
+//	}
+//	
+//
+//}
--- src/main/java/com/spring_boot/example/serviceimpl/UserDaoServiceImpl.java
+++ src/main/java/com/spring_boot/example/serviceimpl/UserDaoServiceImpl.java
@@ -1,51 +1,51 @@
-package com.spring_boot.example.serviceimpl;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-import com.spring_boot.example.dao.UserDao;
-import com.spring_boot.example.entities.User;
-import com.spring_boot.example.service.UserService;
-
-@Service
-@Primary
-public class UserDaoServiceImpl implements UserService{
-
-	@Autowired
-	private UserDao userDao;
-	
-	@Override
-	public User createUser(User user) {
-		// TODO Auto-generated method stub
-		return userDao.createUser(user);
-	}
-
-	@Override
-	public List<User> getAllUser() {
-		// TODO Auto-generated method stub
-		return userDao.getAllUser();
-	}
-
-	@Override
-	public User getUser(int userId) {
-		// TODO Auto-generated method stub
-		return userDao.getUser(userId);
-	}
-
-	@Override
-	public User updateUser(int userId, User newUser) {
-		// TODO Auto-generated method stub
-		return userDao.updateUser(userId, newUser);
-	}
-
-	@Override
-	public void deleteUser(int userId) {
-		// TODO Auto-generated method stub
-		userDao.deleteUser(userId);
-		
-	}
-
-}
+//package com.spring_boot.example.serviceimpl;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Service;
+//
+//import com.spring_boot.example.dao.UserDao;
+//import com.spring_boot.example.entities.User;
+//import com.spring_boot.example.service.UserService;
+//
+//@Service
+//@Primary
+//public class UserDaoServiceImpl implements UserService{
+//
+//	@Autowired
+//	private UserDao userDao;
+//	
+//	@Override
+//	public User createUser(User user) {
+//		// TODO Auto-generated method stub
+//		return userDao.createUser(user);
+//	}
+//
+//	@Override
+//	public List<User> getAllUser() {
+//		// TODO Auto-generated method stub
+//		return userDao.getAllUser();
+//	}
+//
+//	@Override
+//	public User getUser(int userId) {
+//		// TODO Auto-generated method stub
+//		return userDao.getUser(userId);
+//	}
+//
+//	@Override
+//	public User updateUser(int userId, User newUser) {
+//		// TODO Auto-generated method stub
+//		return userDao.updateUser(userId, newUser);
+//	}
+//
+//	@Override
+//	public void deleteUser(int userId) {
+//		// TODO Auto-generated method stub
+//		userDao.deleteUser(userId);
+//		
+//	}
+//
+//}
